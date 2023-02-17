d9870a051bb074f4ec8ab83444f26f0088d63abb
Henrik Kirk <henrik@ece.au.dk>
2023-02-17 09:08:02 +0100

    Changed port number

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ const connect = require('gulp-connect')
 const autoprefixer = require('gulp-autoprefixer')
 
 const root = yargs.argv.root || '.'
-const port = yargs.argv.port || 8000
+const port = yargs.argv.port || 8020
 const host = yargs.argv.host || 'localhost'
 
 const cssLicense = `
